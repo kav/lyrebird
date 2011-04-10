200ca1cd09c41e28d67b3d81523992eaaff237e9
Kav Latiolais <kavlatiolais@gmail.com>
2011-04-10 14:08:09 -0700

    Built basic front end. No OAuth or backend code in place.

---
+++ implementation/lyrebd/app/controllers/signin_controller.rb
@@ -0,0 +1,7 @@
+class SigninController < ApplicationController
+  def index
+    respond_to do |format|
+    	format.html
+    end	
+  end
+end
---
+++ implementation/lyrebd/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lyrebd::Application.config.secret_token = 'ec788209051f70013b0f1f1844bb6d7944ec3aea3e5a9089f04d0b2a63fcc400f04768a63cfb11030f4807d3e195b0856a3fa588f5c2d4b45f154a80bd1adbed'
