acc0a0329a6f61026431e72cb1d52522ad7ebbeb
Kav Latiolais <kavlatiolais@gmail.com>
2011-04-12 21:37:45 -0700

    Fixed cap using internet suggestion for subdir deploy

--- implementation/lyrebd/config/deploy.rb
+++ implementation/lyrebd/config/deploy.rb
@@ -6,6 +6,8 @@ set :repository,  "gitosis@kav.la:lyrebd.git"
 set :scm, :git
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
+set :deploy_subdir, "implementation/lyrebd"
+
 role :web, domain                          # Your HTTP server, Apache/etc
 role :app, domain                          # This may be the same as your `Web` server
 role :db,  domain, :primary => true # This is where Rails migrations will run
@@ -21,3 +23,30 @@ role :db,  domain, :primary => true # This is where Rails migrations will run
      run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
    end
  end
+
+ require 'capistrano/recipes/deploy/strategy/remote_cache'
+
+ class RemoteCacheSubdir < Capistrano::Deploy::Strategy::RemoteCache
+
+   private
+
+   def repository_cache_subdir
+     if configuration[:deploy_subdir] then
+       File.join(repository_cache, configuration[:deploy_subdir])
+     else
+       repository_cache
+     end
+   end
+
+   def copy_repository_cache
+     logger.trace "copying the cached version to #{configuration[:release_path]}"
+     if copy_exclude.empty? 
+       run "cp -RPp #{repository_cache_subdir} #{configuration[:release_path]} && #{mark}"
+     else
+       exclusions = copy_exclude.map { |e| "--exclude=\"#{e}\"" }.join(' ')
+       run "rsync -lrpt #{exclusions} #{repository_cache_subdir}/* #{configuration[:release_path]} && #{mark}"
+     end
+   end
+
+ end
+
